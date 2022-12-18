5d0cf5d1558aeabf6a2a886b86665572e36b81c9
THISISAGOODNAME <yangyj19931231@gmail.com>
2022-12-18 22:32:04 +0800

    Draw a cube

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # hello_bgfx
 GLFW + bgfx demo
 
+![screenshots](screenshots/Snipaste_2022-12-18_21-56-41.png)
+
 ## how to install dependencies
 
 ```shell
@@ -15,4 +17,15 @@ cd bgfx
 msbuild .build\projects\vs2022\bgfx.sln -target:examples\example-01-cubes;tools\shaderc\shaderc -property:Configuration=Release -property:Platform="x64"
 # If you need debug bgfx use
 # msbuild .build\projects\vs2022\bgfx.sln -target:examples\example-01-cubes;tools\shaderc\shaderc -property:Configuration=Debug -property:Platform="x64"
+```
+
+## how to compile shaders
+
+```shell
+mkdir shaders/dx11
+mkdir shaders/spirv
+.\3rdparty\bgfx\.build\win64_vs2022\bin\shadercRelease.exe -f shaders/vs_cubes.sc -o shaders/dx11/vs_cubes.bin -i 3rdparty/bgfx/src --type vertex --platform windows -p vs_5_0 -O 3
+.\3rdparty\bgfx\.build\win64_vs2022\bin\shadercRelease.exe -f shaders/fs_cubes.sc -o shaders/dx11/fs_cubes.bin -i 3rdparty/bgfx/src --type fragment --platform windows -p ps_5_0 -O 3
+.\3rdparty\bgfx\.build\win64_vs2022\bin\shadercRelease.exe -f shaders/vs_cubes.sc -o shaders/spirv/vs_cubes.bin -i 3rdparty/bgfx/src --type vertex -p spirv
+.\3rdparty\bgfx\.build\win64_vs2022\bin\shadercRelease.exe -f shaders/fs_cubes.sc -o shaders/spirv/fs_cubes.bin -i 3rdparty/bgfx/src --type fragment -p spirv
 ```
--- main.cpp
+++ main.cpp
@@ -7,28 +7,44 @@
 #include "GLFW/glfw3native.h"
 #include "bgfx/bgfx.h"
 #include "bgfx/platform.h"
+#include "bx/math.h"
 
-#include <iostream>
+#include <string>
 
 const int WNDW_WIDTH = 1600;
 const int WNDW_HEIGHT = 900;
 
-static bool s_showStats = false;
-
-static void glfw_keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+bgfx::ShaderHandle loadShader(const char *FILENAME)
 {
-    if (key == GLFW_KEY_F1 && action == GLFW_RELEASE)
-        s_showStats = !s_showStats;
+    std::string shaderPath = "???";
+
+    switch (bgfx::getRendererType()) {
+        case bgfx::RendererType::Direct3D11:
+        case bgfx::RendererType::Direct3D12:    shaderPath = "shaders/dx11/"; break;
+        case bgfx::RendererType::Vulkan:        shaderPath = "shaders/spirv/"; break;
+        default:                                shaderPath = "???";
+    }
+
+    shaderPath += FILENAME;
+
+    FILE *file = fopen(shaderPath.c_str(), "rb");
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    const bgfx::Memory *mem = bgfx::alloc(fileSize + 1);
+    fread(mem->data, 1, fileSize, file);
+    mem->data[mem->size - 1] = '\0';
+    fclose(file);
+
+    return bgfx::createShader(mem);
 }
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
-
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(WNDW_WIDTH, WNDW_HEIGHT, "GLFW + bgfx", nullptr, nullptr);
-    glfwSetKeyCallback(window, glfw_keyCallback);
 
     // Call bgfx::renderFrame before bgfx::init to signal to bgfx not to create a render thread.
     // Most graphics APIs must be used on the same thread that created the window.
@@ -43,6 +59,54 @@ int main()
     bgfxInit.resolution.height = WNDW_HEIGHT;
     bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
     bgfx::init(bgfxInit);
+
+    struct PosColorVertex
+    {
+        float x;
+        float y;
+        float z;
+        uint32_t abgr;
+    };
+
+    static PosColorVertex cubeVertices[] =
+            {
+                    {-1.0f,  1.0f,  1.0f, 0xff000000 },
+                    { 1.0f,  1.0f,  1.0f, 0xff0000ff },
+                    {-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+                    { 1.0f, -1.0f,  1.0f, 0xff00ffff },
+                    {-1.0f,  1.0f, -1.0f, 0xffff0000 },
+                    { 1.0f,  1.0f, -1.0f, 0xffff00ff },
+                    {-1.0f, -1.0f, -1.0f, 0xffffff00 },
+                    { 1.0f, -1.0f, -1.0f, 0xffffffff },
+            };
+
+    static const uint16_t cubeTriList[] =
+            {
+                    0, 1, 2,
+                    1, 3, 2,
+                    4, 6, 5,
+                    5, 6, 7,
+                    0, 2, 4,
+                    4, 2, 6,
+                    1, 5, 3,
+                    5, 7, 3,
+                    0, 4, 1,
+                    4, 5, 1,
+                    2, 3, 6,
+                    6, 3, 7,
+            };
+
+    bgfx::VertexLayout pcvDecl;
+    pcvDecl.begin()
+        .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+        .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+    .end();
+    bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(bgfx::makeRef(cubeVertices, sizeof(cubeVertices)), pcvDecl);
+    bgfx::IndexBufferHandle ibh = bgfx::createIndexBuffer(bgfx::makeRef(cubeTriList, sizeof(cubeTriList)));
+
+    bgfx::ShaderHandle vsh = loadShader("vs_cubes.bin");
+    bgfx::ShaderHandle fsh = loadShader("fs_cubes.bin");
+    bgfx::ProgramHandle program = bgfx::createProgram(vsh, fsh, true);
 
     // Rendering Loop
     unsigned int counter = 0;
@@ -59,24 +123,27 @@ int main()
         // This dummy draw call is here to make sure that view 0 is cleared if no other draw calls are submitted to view 0.
         bgfx::touch(0);
 
-        // Use debug font to print information about this example.
-        bgfx::dbgTextClear();
-        bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
-        bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
-        bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
-        bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
-        const bgfx::Stats* stats = bgfx::getStats();
-        bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
-        // Enable stats or debug text.
-        bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
+        const bx::Vec3 at = {0.0f, 0.0f, 0.0f};
+        const bx::Vec3 eye = {0.0f, 0.0f, -5.0f};
+        float view[16];
+        bx::mtxLookAt(view, eye, at);
+        float proj[16];
+        bx::mtxProj(proj, 60.0f, float(WNDW_WIDTH)/float(WNDW_HEIGHT), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+        bgfx::setViewTransform(0, view, proj);
+        float mtx[16];
+        bx::mtxRotateXY(mtx, counter * 0.01f, counter * 0.01f);
+        bgfx::setTransform(mtx);
 
+        bgfx::setVertexBuffer(0, vbh);
+        bgfx::setIndexBuffer(ibh);
+
+        bgfx::submit(0, program);
         bgfx::frame();
 
-        glfwSwapBuffers(window);
         counter++;
     }
 
-    bgfx::shutdown();
+//    bgfx::shutdown();
     glfwTerminate();
     return EXIT_SUCCESS;
 }
